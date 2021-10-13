d22e45fbf0e7fe41bd1b1900ccef93864fac3f12
David White <45552932+DavidjWhite33@users.noreply.github.com>
2021-10-13 12:25:04 +1100

    First commit: test build of EEGLAB compiled version
    Note: as of 13 Oct 2021 this cannot currently use main branch of neurodocker, as matlab mcr is not available. This currently relies on the 20210924-matlabmcr branch - BUT also requires edits to this matlabmcr yaml to include recent matlab runtime versions. Note also, additional dependencies for mcr installation are included in build.sh (curl ca-certificates unzip)

---
+++ recipes/eeglab/build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -e
+
+# this template file builds eeglab and is then used as a docker base image for layer caching
+export toolName='eeglab'
+export toolVersion='2020.0'
+# Don't forget to update version change in README.md!!!!!
+
+if [ "$1" != "" ]; then
+    echo "Entering Debug mode"
+    export debug="true"
+fi
+
+source ../main_setup.sh
+
+neurodocker generate ${neurodocker_buildMode} \
+   --base-image ubuntu:18.04 \
+   --pkg-manager apt \
+   --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
+   --run="chmod +x /usr/bin/ll" \
+   --run="mkdir ${mountPointList}" \
+   --install openjdk-8-jre curl ca-certificates unzip \
+   --matlabmcr version=2020a install_path=/opt/MCRv98 method=binaries  \
+   --workdir /opt/${toolName}-${toolVersion}/ \
+   --copy eeglab2020.0_mcr2020a.tar.gz /opt/${toolName}-${toolVersion}.tar.gz \
+   --run="tar -xzf /opt/${toolName}-${toolVersion}.tar.gz -C /opt/${toolName}-${toolVersion}/ --strip-components 1" \
+   --env LD_LIBRARY_PATH="/usr/lib/x86_64-linux-gnu:/opt/MCRv98/v98/runtime/glnxa64:/opt/MCRv98/v98/bin/glnxa64:/opt/MCRv98/v98/sys/os/glnxa64:/opt/MCRv98/v98/sys/opengl/lib/glnxa64" \
+   --env XAPPLRESDIR=/opt/MCRv98/v98/x11/app-defaults \
+   --env PATH=/opt/${toolName}-${toolVersion}/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
+   --env DEPLOY_BINS=${toolName} \
+   --copy README.md /README.md \
+  > ${imageName}.${neurodocker_buildExt}
+
+if [ "$debug" = "true" ]; then
+   ./../main_build.sh
+fi
+
+#Once you have compiled archive in object storage:
+#--run="curl -fsSL --retry 5 https://objectstorage.us-ashburn-1.oraclecloud.com/eeglab2020.0_mcr2020a.tar.gz \
+#      | tar -xz -C /opt/${toolName}-${toolVersion}/ --strip-components 1" \
