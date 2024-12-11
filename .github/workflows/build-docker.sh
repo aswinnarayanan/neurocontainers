53feeb56cadd6bcbdac3cbed9de2afa0cae8d18f
aswinnarayanan <aswinn89@gmail.com>
2024-12-11 09:16:29 +0000

    Skip singularity

--- .github/workflows/build-docker.sh
+++ .github/workflows/build-docker.sh
@@ -52,51 +52,51 @@ else
     echo "[DEBUG] Changes found"
 fi
 
-# Build singularity container and upload to cache to speed up testing of images:
+# # Build singularity container and upload to cache to speed up testing of images:
 
-#This prevents the sometimes stuck apt process from stopping the build
-if [ -f "/var/lib/apt/lists/lock" ]; then
-  sudo rm /var/lib/apt/lists/lock
-  sudo rm /var/cache/apt/archives/lock
-  sudo rm /var/lib/dpkg/lock*
-fi
+# #This prevents the sometimes stuck apt process from stopping the build
+# if [ -f "/var/lib/apt/lists/lock" ]; then
+#   sudo rm /var/lib/apt/lists/lock
+#   sudo rm /var/cache/apt/archives/lock
+#   sudo rm /var/lib/dpkg/lock*
+# fi
 
-# install apptainer if no singularity executable is available
-if ! command -v singularity &> /dev/null
-then
-    sudo apt-get install -y software-properties-common
-    sudo add-apt-repository -y ppa:apptainer/ppa
-    sudo apt-get update
-    sudo apt-get install -y apptainer 
-fi
+# # install apptainer if no singularity executable is available
+# if ! command -v singularity &> /dev/null
+# then
+#     sudo apt-get install -y software-properties-common
+#     sudo add-apt-repository -y ppa:apptainer/ppa
+#     sudo apt-get update
+#     sudo apt-get install -y apptainer 
+# fi
 
 
-export IMAGE_HOME="/storage/tmp"
+# export IMAGE_HOME="/storage/tmp"
 
-if [ -d "$IMAGE_HOME" ]; then
-  echo "[DEBUG] $IMAGE_HOME exists"
-else
-  echo "[DEBUG] $IMAGE_HOME does not exist. Creating ..."
-  sudo mkdir -p $IMAGE_HOME
-  sudo chmod a+rwx $IMAGE_HOME
-fi
+# if [ -d "$IMAGE_HOME" ]; then
+#   echo "[DEBUG] $IMAGE_HOME exists"
+# else
+#   echo "[DEBUG] $IMAGE_HOME does not exist. Creating ..."
+#   sudo mkdir -p $IMAGE_HOME
+#   sudo chmod a+rwx $IMAGE_HOME
+# fi
 
-echo "saving docker image locally for singularity to convert:"
-# cleanup first
-if [ -f "${IMAGENAME}_${BUILDDATE}.tar" ]; then
-  rm -rf ${IMAGENAME}_${BUILDDATE}.tar
-fi
-docker save $IMAGEID:$SHORT_SHA -o ${IMAGENAME}_${BUILDDATE}.tar
+# echo "saving docker image locally for singularity to convert:"
+# # cleanup first
+# if [ -f "${IMAGENAME}_${BUILDDATE}.tar" ]; then
+#   rm -rf ${IMAGENAME}_${BUILDDATE}.tar
+# fi
+# docker save $IMAGEID:$SHORT_SHA -o ${IMAGENAME}_${BUILDDATE}.tar
 
-if [ -f "$IMAGE_HOME/${IMAGENAME}_${BUILDDATE}.simg" ]; then
-  rm -rf $IMAGE_HOME/${IMAGENAME}_${BUILDDATE}.simg
-fi
-singularity build "$IMAGE_HOME/${IMAGENAME}_${BUILDDATE}.simg" docker-archive://${IMAGENAME}_${BUILDDATE}.tar
+# if [ -f "$IMAGE_HOME/${IMAGENAME}_${BUILDDATE}.simg" ]; then
+#   rm -rf $IMAGE_HOME/${IMAGENAME}_${BUILDDATE}.simg
+# fi
+# singularity build "$IMAGE_HOME/${IMAGENAME}_${BUILDDATE}.simg" docker-archive://${IMAGENAME}_${BUILDDATE}.tar
 
-# cleanup
-if [ -f "${IMAGENAME}_${BUILDDATE}.tar" ]; then
-  rm -rf ${IMAGENAME}_${BUILDDATE}.tar
-fi
+# # cleanup
+# if [ -f "${IMAGENAME}_${BUILDDATE}.tar" ]; then
+#   rm -rf ${IMAGENAME}_${BUILDDATE}.tar
+# fi
 
 # echo "[DEBUG] Attempting upload to Nectar Object Storage ..."
 # rclone copy --progress $IMAGE_HOME/${IMAGENAME}_${BUILDDATE}.simg nectar:/neurodesk/temporary-builds-new
